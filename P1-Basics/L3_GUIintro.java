356c9cbfce6cbb5821c6d7fe1241bed93e0c10a2
whoshyam <shyamk2712@gmail.com>
2023-09-23 02:45:35 +0530

    update

--- P1-Basics/L3_GUIintro.java
+++ P1-Basics/L3_GUIintro.java
@@ -5,9 +5,11 @@ public class L3_GUIintro {
 	public static void main(String[] args) {
 		
 		String name = JOptionPane.showInputDialog("Enter your name");
+		//showInputDialog used for string 
 		JOptionPane.showMessageDialog(null, "Hello "+name);
 		
 		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+		//parseInt convert string into integer
 		JOptionPane.showMessageDialog(null, "You are "+age+" years old");
 		
 		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
