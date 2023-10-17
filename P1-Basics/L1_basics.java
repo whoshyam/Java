c6230a9c9e0c9c65ebefd1b1aee352d9a09463b8
whoshyam <shyamk2712@gmail.com>
2023-10-17 12:38:58 +0530

    java

--- P1-Basics/L1_basics.java
+++ P1-Basics/L1_basics.java
@@ -1,32 +1,31 @@
 public class L1_basics {
 
-    public static void main(String[] args){
-     //!variables
-     int x = 123;
-     double y = 3.14;
-     boolean z = true;
-     char symbol = '@';
-     String name = "Bro";
-     
-     System.out.println("Hello "+name);
+    public static void main(String[] args) {
+        // !variables
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Bro";
 
+        System.out.println("Hello " + name);
 
-     //!swap 2 no.s
+        // !swap 2 no.s
         String x1 = "water";
-		String y1 = "Kool-Aid";
-		String temp;
-		
-		temp = x1;
-		x1=y1;
-		y1=temp;
-		
-		System.out.println("x1: "+x1);
-		System.out.println("y1: "+y1);
+        String y1 = "Kool-Aid";
+        String temp;
 
-        //!arithmetic expressions
-        // expression =     operands & operators
-        // operands =  values, variables, numbers, quantity
-        // operators = + - * / % 
+        temp = x1;
+        x1 = y1;
+        y1 = temp;
+
+        System.out.println("x1: " + x1);
+        System.out.println("y1: " + y1);
+
+        // !arithmetic expressions
+        // expression = operands & operators
+        // operands = values, variables, numbers, quantity
+        // operators = + - * / %
 
     }
-   }
+}
